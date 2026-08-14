59c141d77a3769967053234723df219f2cc849e9
h2-data <haydenleebarns@gmail.com>
2026-08-13 22:55:01 -0500

    An frankly absurd number of SQL changes

--- hos_snf_enrollment_growth.sql
+++ hos_snf_enrollment_growth.sql
@@ -1,39 +1,53 @@
 -- Active: 1780043201174@@127.0.0.1@3306@hos_snf
-DROP VIEW v_hos_enrollment_state_growth;
 
--- What if there WERE nulls? What would I do in that situation? Just kinda, leave them in? Turn them into zeros?
--- ANSWER: If there are nulls that cant be properly managed by COALESCE, filter them out following caluclation with a WHERE clause.
--- Change to CTE + calculation expression.
-
-CREATE VIEW v_hos_enrollment_state_growth AS
+WITH hos_growth AS (
 SELECT
 	`ENROLLMENT STATE`,
 	COUNT(CASE WHEN DATE = '01/2025' THEN `ENROLLMENT STATE` END) AS Jan_2025,
-    COUNT(CASE WHEN DATE = '10/2025' THEN `ENROLLMENT STATE` END) AS Oct_2025,
-	(COUNT(CASE WHEN DATE = '10/2025' THEN `ENROLLMENT STATE` END) - COUNT(CASE WHEN DATE = '01/2025' THEN `ENROLLMENT STATE` END)) AS raw_growth
- FROM hospice_enrollments
+    COUNT(CASE WHEN DATE = '10/2025' THEN `ENROLLMENT STATE` END) AS Oct_2025
+FROM hospice_enrollments
+GROUP BY `ENROLLMENT STATE`
+)
+SELECT
+	`ENROLLMENT STATE`,
+	Jan_2025,
+	Oct_2025,
+	Oct_2025 - Jan_2025 AS raw_growth
+ FROM hos_growth
  GROUP BY `ENROLLMENT STATE`;
- 
-SELECT 
-    * 
-FROM v_hos_enrollment_state_growth
-ORDER BY raw_growth DESC;
 
--- When compared to the millions of Medicare beneficiaries and hundreds of thousands of hospice care enrolles, the growth shown is insignificant.
--- Now I'll run the same code for Skilled Nursing facilities.
+SELECT
+	`ENROLLMENT STATE`,
+	COUNT(*) AS no_of_enrollments
+FROM hospice_enrollments
+WHERE `ENROLLMENT STATE` = 'CA'
+AND `DATE` IN ('01/2025', '10/2025');
 
-DROP VIEW v_snf_enrollment_state_growth;
+-- The California count data matches the Jan_2025 + Oct_2025 data of the previous query.
 
-CREATE VIEW v_snf_enrollment_state_growth AS
+WITH snf_growth AS (
 SELECT
 	`ENROLLMENT STATE`,
 	COUNT(CASE WHEN DATE = '01/2025' THEN `ENROLLMENT STATE` END) AS Jan_2025,
-    COUNT(CASE WHEN DATE = '12/2025' THEN `ENROLLMENT STATE` END) AS Dec_2025,
-	(COUNT(CASE WHEN DATE = '12/2025' THEN `ENROLLMENT STATE` END) - COUNT(CASE WHEN DATE = '01/2025' THEN `ENROLLMENT STATE` END)) AS raw_growth
- FROM snf_enrollments
+    COUNT(CASE WHEN DATE = '12/2025' THEN `ENROLLMENT STATE` END) AS Dec_2025
+FROM snf_enrollments
+GROUP BY `ENROLLMENT STATE`
+)
+SELECT
+	`ENROLLMENT STATE`,
+	Jan_2025,
+	Dec_2025,
+	Dec_2025 - Jan_2025 AS raw_growth
+ FROM snf_growth
  GROUP BY `ENROLLMENT STATE`;
- 
-SELECT 
-    * 
-FROM v_snf_enrollment_state_growth
-ORDER BY raw_growth DESC;
+
+ -- Sanity Check
+
+SELECT
+	`ENROLLMENT STATE`,
+	COUNT(*) AS no_of_enrollments
+FROM snf_enrollments
+WHERE `ENROLLMENT STATE` = 'CA'
+AND `DATE` IN ('01/2025', '12/2025');
+
+-- The California count data matches the Jan_2025 + Oct_2025 data of the previous query.
